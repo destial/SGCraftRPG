89f3230c629f78cb95dbc6460aaa27da86a0227d
destial <destialpvp@gmail.com>
2022-05-19 09:30:17 +0800

    refactor

--- src/main/java/xyz/destiall/sgcraftrpg/listeners/DamageListener.java
+++ src/main/java/xyz/destiall/sgcraftrpg/listeners/DamageListener.java
@@ -1,46 +1,41 @@
 package xyz.destiall.sgcraftrpg.listeners;
 
+import com.sucy.skill.api.event.SkillDamageEvent;
 import com.zenya.damageindicator.event.HologramSpawnEvent;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityRegainHealthEvent;
 import xyz.destiall.sgcraftrpg.SGCraftRPG;
-import xyz.destiall.sgcraftrpg.utils.Formatter;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class DamageListener implements Listener {
-    private final Map<LivingEntity, Double> cache = new ConcurrentHashMap<>();
+    private final Map<LivingEntity, String> cache = new ConcurrentHashMap<>();
     private final SGCraftRPG plugin;
 
     public DamageListener(SGCraftRPG plugin) {
         this.plugin = plugin;
     }
 
-    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
-    public void onDamage(EntityDamageByEntityEvent e) {
-        Entity entity = e.getEntity();
-        if (entity instanceof LivingEntity) {
-            cache.put((LivingEntity) e.getEntity(), e.getDamage());
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onDamage(SkillDamageEvent event) {
+        if (event.getDamager() instanceof Player) {
+            cache.put(event.getTarget(), event.getClassification());
         }
     }
 
-    @EventHandler(priority = EventPriority.MONITOR)
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onHologram(HologramSpawnEvent e) {
         if (e.getEvent() instanceof EntityRegainHealthEvent) return;
-        Double damage = cache.remove(e.getEntity());
-        if (damage == null) return;
-        if (damage < e.getAmount()) {
-            String format = e.getFormat();
-            String critFormat = plugin.getConfig().getString("crit-format");
-            critFormat = Formatter.color(critFormat);
-            critFormat = critFormat.replace("%original%", format);
-            e.setFormat(critFormat);
-        }
+        String classification = cache.remove(e.getEntity());
+        if (classification == null) return;
+        String newFormat = plugin.getConfig().getString("damage-indicator." + classification);
+        if (newFormat == null) return;
+        String format = e.getFormat();
+        e.setFormat(newFormat.replace("%original%", format));
     }
 }
